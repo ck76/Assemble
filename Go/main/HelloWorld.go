fb8a6d0931ccd2f67f1a105e9644e8d6900aa270
chengkun <276128749@qq.com>
2019-11-18 17:42:16 +0800

    gogogogogo

---
+++ Go/main/HelloWorld.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
+
+//http://go-tour-zh.appspot.com/welcome/1  //go语言之旅
---
+++ Go/main/【】数据类型/DataType.go
@@ -0,0 +1,11 @@
+package __数据类型
+
+
+import (
+"fmt"
+"math/rand"
+)
+
+func main() {
+	fmt.Println("My favorite number is", rand.Intn(10))
+}
