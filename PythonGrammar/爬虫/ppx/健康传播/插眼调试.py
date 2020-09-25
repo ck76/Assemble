8079a777e530f373628bacdec892096f58801b6e
chengkun <276128749@qq.com>
2020-09-25 17:54:53 +0800

    就是会失效，反扒机制，kkkkkk，可以请求5次

--- PythonGrammar/爬虫/ppx/健康传播/插眼调试.py
+++ PythonGrammar/爬虫/ppx/健康传播/插眼调试.py
@@ -3,49 +3,42 @@ import urllib
 from urllib.error import HTTPError
 import certifi
 import cryptography
-
+# import 用户个人页视频下载
 import requests
+
+s = '''
+install_id=2374555398325096; ttreq=1$5e844322393483f3a1d4e2d0944a149cc9273d6f; d_ticket=5f8fc87635be7acc88e9a5708a3f046607d02; odin_tt=086849e00d3f955d5541126c9a7dec7ec2db27557fdff128f543fda2466a825f2b1ee2b9585686725c960c1e409df588c405e4b8b8be5754b445f00e4af92c8d; sessionid=158c6ffe4aa1c03f4d8eef9c7487c6a0; sessionid_ss=158c6ffe4aa1c03f4d8eef9c7487c6a0; sid_guard=158c6ffe4aa1c03f4d8eef9c7487c6a0%7C1599353104%7C5184000%7CThu%2C+05-Nov-2020+00%3A45%3A04+GMT; sid_tt=158c6ffe4aa1c03f4d8eef9c7487c6a0; uid_tt=0b8e14f094b02cee966fad4d20de14af; uid_tt_ss=0b8e14f094b02cee966fad4d20de14af
+odin_tt=086849e00d3f955d5541126c9a7dec7ec2db27557fdff128f543fda2466a825f2b1ee2b9585686725c960c1e409df588c405e4b8b8be5754b445f00e4af92c8d; d_ticket=5f8fc87635be7acc88e9a5708a3f046607d02; sid_guard=158c6ffe4aa1c03f4d8eef9c7487c6a0%7C1599353104%7C5184000%7CThu%2C+05-Nov-2020+00%3A45%3A04+GMT; uid_tt=0b8e14f094b02cee966fad4d20de14af; uid_tt_ss=0b8e14f094b02cee966fad4d20de14af; sid_tt=158c6ffe4aa1c03f4d8eef9c7487c6a0; sessionid=158c6ffe4aa1c03f4d8eef9c7487c6a0; sessionid_ss=158c6ffe4aa1c03f4d8eef9c7487c6a0; install_id=2374555398325096; ttreq=1$5e844322393483f3a1d4e2d0944a149cc9273d6f
+'''
 headers = {
     "Host": "is.snssdk.com",
-    # "Cookie": "odin_tt=086849e00d3f955d5541126c9a7dec7ec2db27557fdff128f543fda2466a825f2b1ee2b9585686725c960c1e409df588c405e4b8b8be5754b445f00e4af92c8d; d_ticket=5f8fc87635be7acc88e9a5708a3f046607d02; sid_guard=158c6ffe4aa1c03f4d8eef9c7487c6a0%7C1599353104%7C5184000%7CThu%2C+05-Nov-2020+00%3A45%3A04+GMT; uid_tt=0b8e14f094b02cee966fad4d20de14af; uid_tt_ss=0b8e14f094b02cee966fad4d20de14af; sid_tt=158c6ffe4aa1c03f4d8eef9c7487c6a0; sessionid=158c6ffe4aa1c03f4d8eef9c7487c6a0; sessionid_ss=158c6ffe4aa1c03f4d8eef9c7487c6a0; install_id=2374555398325096; ttreq=1$5e844322393483f3a1d4e2d0944a149cc9273d6f",
     "Cookie": "odin_tt=086849e00d3f955d5541126c9a7dec7ec2db27557fdff128f543fda2466a825f2b1ee2b9585686725c960c1e409df588c405e4b8b8be5754b445f00e4af92c8d; d_ticket=5f8fc87635be7acc88e9a5708a3f046607d02; sid_guard=158c6ffe4aa1c03f4d8eef9c7487c6a0%7C1599353104%7C5184000%7CThu%2C+05-Nov-2020+00%3A45%3A04+GMT; uid_tt=0b8e14f094b02cee966fad4d20de14af; uid_tt_ss=0b8e14f094b02cee966fad4d20de14af; sid_tt=158c6ffe4aa1c03f4d8eef9c7487c6a0; sessionid=158c6ffe4aa1c03f4d8eef9c7487c6a0; sessionid_ss=158c6ffe4aa1c03f4d8eef9c7487c6a0; install_id=2374555398325096; ttreq=1$5e844322393483f3a1d4e2d0944a149cc9273d6f",
     "x-tt-token": "00158c6ffe4aa1c03f4d8eef9c7487c6a06e3c8a43680206e28de7a4277c8847ec1489eef72ab80aa4b50a96f145a84a558",
     "x-ss-cookie": "install_id=2374555398325096; ttreq=1$5e844322393483f3a1d4e2d0944a149cc9273d6f; d_ticket=5f8fc87635be7acc88e9a5708a3f046607d02; odin_tt=086849e00d3f955d5541126c9a7dec7ec2db27557fdff128f543fda2466a825f2b1ee2b9585686725c960c1e409df588c405e4b8b8be5754b445f00e4af92c8d; sessionid=158c6ffe4aa1c03f4d8eef9c7487c6a0; sessionid_ss=158c6ffe4aa1c03f4d8eef9c7487c6a0; sid_guard=158c6ffe4aa1c03f4d8eef9c7487c6a0%7C1599353104%7C5184000%7CThu%2C+05-Nov-2020+00%3A45%3A04+GMT; sid_tt=158c6ffe4aa1c03f4d8eef9c7487c6a0; uid_tt=0b8e14f094b02cee966fad4d20de14af; uid_tt_ss=0b8e14f094b02cee966fad4d20de14af",
-    "tt-request-time": "1600951792975",
-    # "user-agent": "Super 3.0.6 rv:3.0.6.80 (iPhone; iOS 13.5.1; zh_CN) Cronet",
+    "tt-request-time": "1601025983552",
     "user-agent": "Super 3.0.6 rv:3.0.6.80 (iPhone; iOS 13.5.1; zh_CN) Cronet",
     "sdk-version": "2",
     "passport-sdk-version": "5.12.0",
     "x-ss-dp": "1319",
-    "x-tt-trace-id": "00-c029b6c70d97fa12ce819a71fe180527-c029b6c70d97fa12-01",
-    "x-khronos": "1600951792",
-    "x-gorgon": "8402a00200006406593d068d8d60974515802bd3cec1c9289c00"
+    "x-tt-trace-id": "00-c495c5b50d97fa12ce819a7cb3a70527-c495c5b50d97fa12-01",
+    "x-khronos": "1601025982",
+    "x-gorgon": "8402e0590000033f82e34e335363d60fe874db85e53a8ec5b2a5"
 }
-# headers = {
-#     'Cookie': 'sessionid=158c6ffe4aa1c03f4d8eef9c7487c6a0',
-#     "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1",
-# }
+
 熟人志 = "https://is.snssdk.com/bds/user/publish_list/?iid=2374555398325096&resolution=1242*2208&os_version=13.5.1&app_name=super&channel=App%20Store&idfa=1ABF0A44-4CBE-4191-B8E6-154836171658&device_platform=iphone&mac_address=02:00:00:00:00:00&vid=86A0335D-BB60-42CC-A461-72D1D67C6DE1&openudid=01942689b4d6f20e58ba3abb334a86fcf32d187c&device_type=iPhone%207%20Plus&idfv=86A0335D-BB60-42CC-A461-72D1D67C6DE1&version_code=3.0.6&ac=WIFI&device_id=2673605010266535&aid=1319&update_version_code=30680&count=20&user_id=1947990820141172&api_version=1&direction=1"
-ss="https://is.snssdk.com/bds/user/user_profile/?iid=2374555398325096&resolution=1242*2208&os_version=13.5.1&app_name=super&channel=App%20Store&idfa=1ABF0A44-4CBE-4191-B8E6-154836171658&device_platform=iphone&mac_address=02:00:00:00:00:00&vid=86A0335D-BB60-42CC-A461-72D1D67C6DE1&openudid=01942689b4d6f20e58ba3abb334a86fcf32d187c&device_type=iPhone%207%20Plus&idfv=86A0335D-BB60-42CC-A461-72D1D67C6DE1&version_code=3.0.6&ac=WIFI&device_id=2673605010266535&aid=1319&update_version_code=30680&user_id=1226655901687588"
-url = "https://is.snssdk.com/bds/ward/list/?iid=2374555398325096&resolution=1242*2208&os_version=13.5.1&app_name=super&channel=App%20Store&idfa=1ABF0A44-4CBE-4191-B8E6-154836171658&device_platform=iphone&mac_address=02:00:00:00:00:00&vid=86A0335D-BB60-42CC-A461-72D1D67C6DE1&openudid=01942689b4d6f20e58ba3abb334a86fcf32d187c&device_type=iPhone%207%20Plus&idfv=86A0335D-BB60-42CC-A461-72D1D67C6DE1&version_code=3.0.6&ac=WIFI&device_id=2673605010266535&aid=1319&update_version_code=30680&user_id=179942927110947&count=20"
+ss = "https://is.snssdk.com/bds/user/user_profile/?iid=2374555398325096&resolution=1242*2208&os_version=13.5.1&app_name=super&channel=App%20Store&idfa=1ABF0A44-4CBE-4191-B8E6-154836171658&device_platform=iphone&mac_address=02:00:00:00:00:00&vid=86A0335D-BB60-42CC-A461-72D1D67C6DE1&openudid=01942689b4d6f20e58ba3abb334a86fcf32d187c&device_type=iPhone%207%20Plus&idfv=86A0335D-BB60-42CC-A461-72D1D67C6DE1&version_code=3.0.6&ac=WIFI&device_id=2673605010266535&aid=1319&update_version_code=30680&user_id=1226655901687588"
+url2 = "https://is.snssdk.com/bds/ward/list/?iid=2374555398325096&resolution=1242*2208&os_version=13.5.1&app_name=super&channel=App%20Store&idfa=1ABF0A44-4CBE-4191-B8E6-154836171658&device_platform=iphone&mac_address=02:00:00:00:00:00&vid=86A0335D-BB60-42CC-A461-72D1D67C6DE1&openudid=01942689b4d6f20e58ba3abb334a86fcf32d187c&device_type=iPhone%207%20Plus&idfv=86A0335D-BB60-42CC-A461-72D1D67C6DE1&version_code=3.0.6&ac=WIFI&device_id=2673605010266535&aid=1319&update_version_code=30680&user_id=179942927110947&count=20"
+loadmore = "https://is.snssdk.com/bds/user/publish_list/?iid=2374555398325096&resolution=1242*2208&os_version=13.5.1&app_name=super&channel=App%20Store&idfa=1ABF0A44-4CBE-4191-B8E6-154836171658&device_platform=iphone&mac_address=02:00:00:00:00:00&vid=86A0335D-BB60-42CC-A461-72D1D67C6DE1&openudid=01942689b4d6f20e58ba3abb334a86fcf32d187c&device_type=iPhone%207%20Plus&idfv=86A0335D-BB60-42CC-A461-72D1D67C6DE1&version_code=3.0.6&ac=WIFI&device_id=2673605010266535&aid=1319&update_version_code=30680&count=20&user_id=1947990820141172&api_version=1&direction=2&cursor=1600863792251060"
 
-r = requests.get(熟人志, headers=headers)
-r.encoding="utf-8"
-# print(r.json())
-# print(r.content)
-# print(r.text)
-
+chayan="https://is.snssdk.com/bds/ward/list/?iid=2374555398325096&resolution=1242*2208&os_version=13.5.1&app_name=super&channel=App%20Store&idfa=1ABF0A44-4CBE-4191-B8E6-154836171658&device_platform=iphone&mac_address=02:00:00:00:00:00&vid=86A0335D-BB60-42CC-A461-72D1D67C6DE1&openudid=01942689b4d6f20e58ba3abb334a86fcf32d187c&device_type=iPhone%207%20Plus&idfv=86A0335D-BB60-42CC-A461-72D1D67C6DE1&version_code=3.0.6&ac=WIFI&device_id=2673605010266535&aid=1319&update_version_code=30680&user_id=179942927110947&count=20"
 
 class RequestSpider(object):
     def __init__(self):
-        # url = 'https://www.baidu.com'
-        url=熟人志
-        # headers = {
-        #     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.67 Safari/537.36'
-        # }
+        url = 熟人志
         self.response = requests.get(url, headers=headers)
 
     def run(self):
+        print("text" + self.response.text)
         data = self.response.content
         print(data)
 
--- PythonGrammar/爬虫/ppx/健康传播/用户个人页视频下载.py
+++ PythonGrammar/爬虫/ppx/健康传播/用户个人页视频下载.py
@@ -13,90 +13,79 @@ import requests
 # 2找春风
 # 3绿色观察
 # 4长寿秘籍
+# 健康观察(分身)
+# 健康观察
+# 熟人志
+switch_s = [False, False, False, False, False, False, True, False, False, False, False, ]
 
-path_save_0 = "/Volumes/TOSHIBA/ppx/插眼/"
-path_save_1 = "/Volumes/TOSHIBA/ppx/健康传播/"
-path_save_2 = "/Volumes/TOSHIBA/ppx/找春风/"
-path_save_3 = "/Volumes/TOSHIBA/ppx/绿色观察/"
-path_save_4 = "/Volumes/TOSHIBA/ppx/长寿秘籍/"
-path_save_5 = ""
-path_save_6 = ""
+path_save_0 = "/Volumes/TOSHIBA/ppx/【0】插眼/"
+path_save_1 = "/Volumes/TOSHIBA/ppx/【1】健康传播/"
+path_save_2 = "/Volumes/TOSHIBA/ppx/【2】找春风/"
+path_save_3 = "/Volumes/TOSHIBA/ppx/【3】绿色观察/"
+path_save_4 = "/Volumes/TOSHIBA/ppx/【4】长寿秘籍/"
+path_save_5 = "/Volumes/TOSHIBA/ppx/【5】健康观察(分身)/"
+path_save_6 = "/Volumes/TOSHIBA/ppx/【6】健康观察/"
+path_save_7 = "/Volumes/TOSHIBA/ppx/【7】熟人志/"
+path_save_8 = ""
+path_save_9 = ""
+path_save_10 = ""
 # 目标视频存储位置
-path_save_s = [path_save_0, path_save_1, path_save_2, path_save_3, path_save_4, path_save_5, path_save_6, ]
+path_save_s = [path_save_0, path_save_1, path_save_2, path_save_3, path_save_4, path_save_5, path_save_6, path_save_7,
+               path_save_8, path_save_9, path_save_10]
 
 init_url_0 = "https://is.snssdk.com/bds/ward/list/?iid=2374555398325096&resolution=1242*2208&os_version=13.5.1&app_name=super&channel=App%20Store&idfa=1ABF0A44-4CBE-4191-B8E6-154836171658&device_platform=iphone&mac_address=02:00:00:00:00:00&vid=86A0335D-BB60-42CC-A461-72D1D67C6DE1&openudid=01942689b4d6f20e58ba3abb334a86fcf32d187c&device_type=iPhone%207%20Plus&idfv=86A0335D-BB60-42CC-A461-72D1D67C6DE1&version_code=3.0.6&ac=WIFI&device_id=2673605010266535&aid=1319&update_version_code=30680&user_id=179942927110947&count=20"
 init_url_1 = "https://is.snssdk.com/bds/user/publish_list/?iid=2937491619330999&resolution=1242*2208&os_version=13.5.1&app_name=super&channel=App%20Store&idfa=6E007FDB-BEAC-493A-B75D-B45F76AEA6BD&device_platform=iphone&mac_address=02:00:00:00:00:00&vid=6248603F-E50C-444D-B928-543FB93C1F8C&openudid=01942689b4d6f20e58ba3abb334a86fcf32d187c&device_type=iPhone%207%20Plus&idfv=6248603F-E50C-444D-B928-543FB93C1F8C&version_code=2.9.8&ac=WIFI&device_id=2673605010266535&aid=1319&update_version_code=29880&count=20&user_id=1591688795264663&api_version=1&direction=1"
 init_url_2 = "https://is.snssdk.com/bds/user/publish_list/?iid=2374555398325096&resolution=1242*2208&os_version=13.5.1&app_name=super&channel=App%20Store&idfa=1ABF0A44-4CBE-4191-B8E6-154836171658&device_platform=iphone&mac_address=02:00:00:00:00:00&vid=86A0335D-BB60-42CC-A461-72D1D67C6DE1&openudid=01942689b4d6f20e58ba3abb334a86fcf32d187c&device_type=iPhone%207%20Plus&idfv=86A0335D-BB60-42CC-A461-72D1D67C6DE1&version_code=3.0.6&ac=WIFI&device_id=2673605010266535&aid=1319&update_version_code=30680&count=20&user_id=4358067917166531&api_version=1&direction=1"
 init_url_3 = "https://is.snssdk.com/bds/user/publish_list/?iid=2374555398325096&resolution=1242*2208&os_version=13.5.1&app_name=super&channel=App%20Store&idfa=1ABF0A44-4CBE-4191-B8E6-154836171658&device_platform=iphone&mac_address=02:00:00:00:00:00&vid=86A0335D-BB60-42CC-A461-72D1D67C6DE1&openudid=01942689b4d6f20e58ba3abb334a86fcf32d187c&device_type=iPhone%207%20Plus&idfv=86A0335D-BB60-42CC-A461-72D1D67C6DE1&version_code=3.0.6&ac=WIFI&device_id=2673605010266535&aid=1319&update_version_code=30680&count=20&user_id=6058032285&api_version=1&direction=1"
 init_url_4 = "https://is.snssdk.com/bds/user/publish_list/?iid=2374555398325096&resolution=1242*2208&os_version=13.5.1&app_name=super&channel=App%20Store&idfa=1ABF0A44-4CBE-4191-B8E6-154836171658&device_platform=iphone&mac_address=02:00:00:00:00:00&vid=86A0335D-BB60-42CC-A461-72D1D67C6DE1&openudid=01942689b4d6f20e58ba3abb334a86fcf32d187c&device_type=iPhone%207%20Plus&idfv=86A0335D-BB60-42CC-A461-72D1D67C6DE1&version_code=3.0.6&ac=WIFI&device_id=2673605010266535&aid=1319&update_version_code=30680&count=20&user_id=1591688795264663&api_version=1&direction=1"
-init_url_5 = ""
-init_url_6 = ""
+init_url_5 = "https://ib.snssdk.com/bds/user/publish_list/?iid=2374555398325096&resolution=1242*2208&os_version=13.5.1&app_name=super&channel=App%20Store&idfa=1ABF0A44-4CBE-4191-B8E6-154836171658&device_platform=iphone&mac_address=02:00:00:00:00:00&vid=86A0335D-BB60-42CC-A461-72D1D67C6DE1&openudid=01942689b4d6f20e58ba3abb334a86fcf32d187c&device_type=iPhone%207%20Plus&idfv=86A0335D-BB60-42CC-A461-72D1D67C6DE1&version_code=3.0.6&ac=WIFI&device_id=2673605010266535&aid=1319&update_version_code=30680&count=20&user_id=2334989129615431&api_version=1&direction=1"
+init_url_6 = "https://ib.snssdk.com/bds/user/publish_list/?iid=2374555398325096&resolution=1242*2208&os_version=13.5.1&app_name=super&channel=App%20Store&idfa=1ABF0A44-4CBE-4191-B8E6-154836171658&device_platform=iphone&mac_address=02:00:00:00:00:00&vid=86A0335D-BB60-42CC-A461-72D1D67C6DE1&openudid=01942689b4d6f20e58ba3abb334a86fcf32d187c&device_type=iPhone%207%20Plus&idfv=86A0335D-BB60-42CC-A461-72D1D67C6DE1&version_code=3.0.6&ac=WIFI&device_id=2673605010266535&aid=1319&update_version_code=30680&count=20&user_id=1226655901687588&api_version=1&direction=1"
+init_url_7 = "https://is.snssdk.com/bds/user/publish_list/?iid=2374555398325096&resolution=1242*2208&os_version=13.5.1&app_name=super&channel=App%20Store&idfa=1ABF0A44-4CBE-4191-B8E6-154836171658&device_platform=iphone&mac_address=02:00:00:00:00:00&vid=86A0335D-BB60-42CC-A461-72D1D67C6DE1&openudid=01942689b4d6f20e58ba3abb334a86fcf32d187c&device_type=iPhone%207%20Plus&idfv=86A0335D-BB60-42CC-A461-72D1D67C6DE1&version_code=3.0.6&ac=WIFI&device_id=2673605010266535&aid=1319&update_version_code=30680&count=20&user_id=1947990820141172&api_version=1&direction=1"
+init_url_8 = ""
+init_url_9 = ""
+init_url_10 = ""
 # url
-init_url_s = [init_url_0, init_url_1, init_url_2, init_url_3, init_url_4, init_url_5, init_url_6, ]
+init_url_s = [init_url_0, init_url_1, init_url_2, init_url_3, init_url_4, init_url_5, init_url_6, init_url_7,
+              init_url_8, init_url_9, init_url_10]
 
 path_yixiazai_0 = "/Volumes/TOSHIBA/ppx/插眼.txt"
 path_yixiazai_1 = "/Volumes/TOSHIBA/ppx/健康传播.txt"
 path_yixiazai_2 = "/Volumes/TOSHIBA/ppx/找春风.txt"
 path_yixiazai_3 = "/Volumes/TOSHIBA/ppx/绿色观察.txt"
 path_yixiazai_4 = "/Volumes/TOSHIBA/ppx/长寿秘籍.txt"
-path_yixiazai_5 = ""
-path_yixiazai_6 = ""
+path_yixiazai_5 = "/Volumes/TOSHIBA/ppx/健康观察(分身).txt"
+path_yixiazai_6 = "/Volumes/TOSHIBA/ppx/健康观察.txt"
+path_yixiazai_7 = "/Volumes/TOSHIBA/ppx/熟人志.txt"
+path_yixiazai_8 = ""
+path_yixiazai_9 = ""
+path_yixiazai_10 = ""
 
 # 已下载的目录文件
 path_yixiazai_s = [path_yixiazai_0, path_yixiazai_1, path_yixiazai_2, path_yixiazai_3, path_yixiazai_4, path_yixiazai_5,
-                   path_yixiazai_6, ]
-cookie_dict = {
-    "odin_tt": "086849e00d3f955d5541126c9a7dec7ec2db27557fdff128f543fda2466a825f2b1ee2b9585686725c960c1e409df588c405e4b8b8be5754b445f00e4af92c8d",
-    "d_ticket": "5f8fc87635be7acc88e9a5708a3f046607d02",
-    "sid_guard": "158c6ffe4aa1c03f4d8eef9c7487c6a0%7C1599353104%7C5184000%7CThu%2C+05-Nov-2020+00%3A45%3A04+GMT",
-    "uid_tt": "0b8e14f094b02cee966fad4d20de14af",
-    "uid_tt_ss": "0b8e14f094b02cee966fad4d20de14af",
-    "sid_t": "158c6ffe4aa1c03f4d8eef9c7487c6a0",
-    "sessionid": "158c6ffe4aa1c03f4d8eef9c7487c6a0",
-    "sessionid_ss": "158c6ffe4aa1c03f4d8eef9c7487c6a0",
-    "install_id": "2374555398325096",
-    "ttreq": "1$5e844322393483f3a1d4e2d0944a149cc9273d6f",
-}
-# headers = {
-#     # 'User-Agent': "Super 3.0.6 rv:3.0.6.80 (iPhone; iOS 13.5.1; zh_CN) Cronet",
-#     # 'Cookie': "odin_tt=086849e00d3f955d5541126c9a7dec7ec2db27557fdff128f543fda2466a825f2b1ee2b9585686725c960c1e409df588c405e4b8b8be5754b445f00e4af92c8d;d_ticket=5f8fc87635be7acc88e9a5708a3f046607d02;sid_guard=158c6ffe4aa1c03f4d8eef9c7487c6a0%7C1599353104%7C5184000%7CThu%2C+05-Nov-2020+00%3A45%3A04+GMT;uid_tt=0b8e14f094b02cee966fad4d20de14af;uid_tt_ss=0b8e14f094b02cee966fad4d20de14af;sid_tt=158c6ffe4aa1c03f4d8eef9c7487c6a0;sessionid=158c6ffe4aa1c03f4d8eef9c7487c6a0;sessionid_ss=158c6ffe4aa1c03f4d8eef9c7487c6a0;install_id=2374555398325096;ttreq=1$5e844322393483f3a1d4e2d0944a149cc9273d6f",
-#     # "Cookie": "sessionid=158c6ffe4aa1c03f4d8eef9c7487c6a0;sessionid_ss=158c6ffe4aa1c03f4d8eef9c7487c6a0",
-#     # "Cookie": cookie,
-#     # "x-tt-token": "00158c6ffe4aa1c03f4d8eef9c7487c6a0d2b52ee1884e982c32b18e9d5172737e44ec74a178c431f10f741b97e487655836",
-#     # "x-ss-cookie": "install_id=2374555398325096;ttreq=1$5e844322393483f3a1d4e2d0944a149cc9273d6f;d_ticket=5f8fc87635be7acc88e9a5708a3f046607d02;odin_tt=086849e00d3f955d5541126c9a7dec7ec2db27557fdff128f543fda2466a825f2b1ee2b9585686725c960c1e409df588c405e4b8b8be5754b445f00e4af92c8d;sessionid=158c6ffe4aa1c03f4d8eef9c7487c6a0;sessionid_ss=158c6ffe4aa1c03f4d8eef9c7487c6a0;sid_guard=158c6ffe4aa1c03f4d8eef9c7487c6a0%7C1599353104%7C5184000%7CThu%2C+05-Nov-2020+00%3A45%3A04+GMT;sid_tt=158c6ffe4aa1c03f4d8eef9c7487c6a0;uid_tt=0b8e14f094b02cee966fad4d20de14af;uid_tt_ss=0b8e14f094b02cee966fad4d20de14af"
-# }
+                   path_yixiazai_6, path_yixiazai_7, path_yixiazai_8, path_yixiazai_9, path_yixiazai_10]
+
 headers = {
-    "Host": "iu.snssdk.com",
-    # "Cookie": "odin_tt=086849e00d3f955d5541126c9a7dec7ec2db27557fdff128f543fda2466a825f2b1ee2b9585686725c960c1e409df588c405e4b8b8be5754b445f00e4af92c8d; d_ticket=5f8fc87635be7acc88e9a5708a3f046607d02; sid_guard=158c6ffe4aa1c03f4d8eef9c7487c6a0%7C1599353104%7C5184000%7CThu%2C+05-Nov-2020+00%3A45%3A04+GMT; uid_tt=0b8e14f094b02cee966fad4d20de14af; uid_tt_ss=0b8e14f094b02cee966fad4d20de14af; sid_tt=158c6ffe4aa1c03f4d8eef9c7487c6a0; sessionid=158c6ffe4aa1c03f4d8eef9c7487c6a0; sessionid_ss=158c6ffe4aa1c03f4d8eef9c7487c6a0; install_id=2374555398325096; ttreq=1$5e844322393483f3a1d4e2d0944a149cc9273d6f",
+    "Host": "is.snssdk.com",
     "Cookie": "odin_tt=086849e00d3f955d5541126c9a7dec7ec2db27557fdff128f543fda2466a825f2b1ee2b9585686725c960c1e409df588c405e4b8b8be5754b445f00e4af92c8d; d_ticket=5f8fc87635be7acc88e9a5708a3f046607d02; sid_guard=158c6ffe4aa1c03f4d8eef9c7487c6a0%7C1599353104%7C5184000%7CThu%2C+05-Nov-2020+00%3A45%3A04+GMT; uid_tt=0b8e14f094b02cee966fad4d20de14af; uid_tt_ss=0b8e14f094b02cee966fad4d20de14af; sid_tt=158c6ffe4aa1c03f4d8eef9c7487c6a0; sessionid=158c6ffe4aa1c03f4d8eef9c7487c6a0; sessionid_ss=158c6ffe4aa1c03f4d8eef9c7487c6a0; install_id=2374555398325096; ttreq=1$5e844322393483f3a1d4e2d0944a149cc9273d6f",
-    "x-tt-token": "00158c6ffe4aa1c03f4d8eef9c7487c6a0d2b52ee1884e982c32b18e9d5172737e44ec74a178c431f10f741b97e487655836",
+    "x-tt-token": "00158c6ffe4aa1c03f4d8eef9c7487c6a06e3c8a43680206e28de7a4277c8847ec1489eef72ab80aa4b50a96f145a84a558",
     "x-ss-cookie": "install_id=2374555398325096; ttreq=1$5e844322393483f3a1d4e2d0944a149cc9273d6f; d_ticket=5f8fc87635be7acc88e9a5708a3f046607d02; odin_tt=086849e00d3f955d5541126c9a7dec7ec2db27557fdff128f543fda2466a825f2b1ee2b9585686725c960c1e409df588c405e4b8b8be5754b445f00e4af92c8d; sessionid=158c6ffe4aa1c03f4d8eef9c7487c6a0; sessionid_ss=158c6ffe4aa1c03f4d8eef9c7487c6a0; sid_guard=158c6ffe4aa1c03f4d8eef9c7487c6a0%7C1599353104%7C5184000%7CThu%2C+05-Nov-2020+00%3A45%3A04+GMT; sid_tt=158c6ffe4aa1c03f4d8eef9c7487c6a0; uid_tt=0b8e14f094b02cee966fad4d20de14af; uid_tt_ss=0b8e14f094b02cee966fad4d20de14af",
-    "tt-request-time": "1600768307278",
-    # "user-agent": "Super 3.0.6 rv:3.0.6.80 (iPhone; iOS 13.5.1; zh_CN) Cronet",
-    "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1",
+    "tt-request-time": "1601025119118",
+    "user-agent": "Super 3.0.6 rv:3.0.6.80 (iPhone; iOS 13.5.1; zh_CN) Cronet",
     "sdk-version": "2",
     "passport-sdk-version": "5.12.0",
     "x-ss-dp": "1319",
-    "x-tt-trace-id": "00-b539f1c50d97fa12ce819a7d46e70527-b539f1c50d97fa12-01",
-    "x-khronos": "1600768304",
-    "x-gorgon": "8402404700007b2b225643b32ce0e6f220f39e992a3287b79169"
+    "x-tt-trace-id": "00-c48895000d97fa12ce819a77366f0527-c48895000d97fa12-01",
+    "x-khronos": "1601025118",
+    "x-gorgon": "8402409e0000c0f5998858494120be40df8599996a777ac01999"
 }
 
 
 def get_download_urls(path_save, init_url, yixiazai, origin_video_download_urls, origin_video_id_s):
-    print(path_save)
-    print(init_url)
+    print("path_save: " + path_save)
+    print("init_url: " + init_url)
     requests.packages.urllib3.disable_warnings()
-    r = requests.get(init_url, headers=headers, verify=False, timeout=10)
-    # print(type(r.headers))
-    # for item in r.headers:
-    #     print(item)
-    # print(r.status_code)
-    # print(r.cookies)
-    # print(r.ok)
-    # print(r.raw)
-    print(r.text)
-    print(r.content)
+    r = requests.get(init_url, headers=headers)
+    r.encoding = "utf-8"
     r_json = json.loads(r.text)
     data_s = r_json.get("data").get("data")
     for cell in data_s:
@@ -122,7 +111,7 @@ def get_download_urls(path_save, init_url, yixiazai, origin_video_download_urls,
     if cursor.get("has_more") == True:
         cursor = cursor.get("loadmore_cursor")
         init_url = init_url.replace("direction=1", "direction=2") + "&cursor=" + str(cursor)
-        print(init_url)
+        print("loadmore "+init_url)
         get_download_urls(path_save, init_url, yixiazai, origin_video_download_urls, origin_video_id_s)
 
 
@@ -133,15 +122,15 @@ def start():
     path_yixiazai = ""
     try:
         print()
-        for index in range(0, len(path_save_s)):
-            if index == 0:
+        for index in range(0, len(switch_s)):
+            # 清除上一个循环的影响
+            origin_video_download_urls = []
+            origin_video_id_s = []
+            yixiazai = []
+            path_yixiazai = ""
+
+            if not switch_s[index]:
                 continue
-            # if index == 1:
-            #     continue
-            # if index == 2:
-            #     continue
-            # if index == 3:
-            #     continue
             path_save = path_save_s[index]
             if path_save == "":
                 continue
