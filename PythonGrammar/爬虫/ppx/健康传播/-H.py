73b9e9e959e270a024e17026e49bbc0edd5ced12
chengkun <276128749@qq.com>
2020-09-25 18:28:07 +0800

    我服了，不搞了，每次弄几个加密的会变得参数这怎么搞，没法搞，改天想别的办法，比如自动化上下滑动屏幕保存所有的返回的json文件，没别的办法了，因为参数经常失效……

--- PythonGrammar/爬虫/ppx/健康传播/-H.py
+++ PythonGrammar/爬虫/ppx/健康传播/-H.py
@@ -10,18 +10,26 @@ s_4="curl -H 'Host: is.snssdk.com' -H 'Cookie: odin_tt=086849e00d3f955d5541126c9
 s_5="curl -H 'Host: is.snssdk.com' -H 'Cookie: odin_tt=086849e00d3f955d5541126c9a7dec7ec2db27557fdff128f543fda2466a825f2b1ee2b9585686725c960c1e409df588c405e4b8b8be5754b445f00e4af92c8d; d_ticket=5f8fc87635be7acc88e9a5708a3f046607d02; sid_guard=158c6ffe4aa1c03f4d8eef9c7487c6a0%7C1599353104%7C5184000%7CThu%2C+05-Nov-2020+00%3A45%3A04+GMT; uid_tt=0b8e14f094b02cee966fad4d20de14af; uid_tt_ss=0b8e14f094b02cee966fad4d20de14af; sid_tt=158c6ffe4aa1c03f4d8eef9c7487c6a0; sessionid=158c6ffe4aa1c03f4d8eef9c7487c6a0; sessionid_ss=158c6ffe4aa1c03f4d8eef9c7487c6a0; install_id=2374555398325096; ttreq=1$5e844322393483f3a1d4e2d0944a149cc9273d6f' -H 'x-tt-token: 00158c6ffe4aa1c03f4d8eef9c7487c6a06e3c8a43680206e28de7a4277c8847ec1489eef72ab80aa4b50a96f145a84a558' -H 'x-ss-cookie: install_id=2374555398325096; ttreq=1$5e844322393483f3a1d4e2d0944a149cc9273d6f; d_ticket=5f8fc87635be7acc88e9a5708a3f046607d02; odin_tt=086849e00d3f955d5541126c9a7dec7ec2db27557fdff128f543fda2466a825f2b1ee2b9585686725c960c1e409df588c405e4b8b8be5754b445f00e4af92c8d; sessionid=158c6ffe4aa1c03f4d8eef9c7487c6a0; sessionid_ss=158c6ffe4aa1c03f4d8eef9c7487c6a0; sid_guard=158c6ffe4aa1c03f4d8eef9c7487c6a0%7C1599353104%7C5184000%7CThu%2C+05-Nov-2020+00%3A45%3A04+GMT; sid_tt=158c6ffe4aa1c03f4d8eef9c7487c6a0; uid_tt=0b8e14f094b02cee966fad4d20de14af; uid_tt_ss=0b8e14f094b02cee966fad4d20de14af' -H 'tt-request-time: 1601026657469' -H 'user-agent: Super 3.0.6 rv:3.0.6.80 (iPhone; iOS 13.5.1; zh_CN) Cronet' -H 'sdk-version: 2' -H 'passport-sdk-version: 5.12.0' -H 'x-ss-dp: 1319' -H 'x-tt-trace-id: 00-c4a00e370d97fa12ce819a7344b90527-c4a00e370d97fa12-01' -H 'x-khronos: 1601026656' -H 'x-gorgon: 8402207d00004e5ef4e00545f1f990a8ed770f2c383ebab5c06d' --compressed 'https://is.snssdk.com/bds/user/publish_list/?iid=2374555398325096&resolution=1242*2208&os_version=13.5.1&app_name=super&channel=App%20Store&idfa=1ABF0A44-4CBE-4191-B8E6-154836171658&device_platform=iphone&mac_address=02:00:00:00:00:00&vid=86A0335D-BB60-42CC-A461-72D1D67C6DE1&openudid=01942689b4d6f20e58ba3abb334a86fcf32d187c&device_type=iPhone%207%20Plus&idfv=86A0335D-BB60-42CC-A461-72D1D67C6DE1&version_code=3.0.6&ac=WIFI&device_id=2673605010266535&aid=1319&update_version_code=30680&count=20&user_id=1947990820141172&api_version=1&direction=1'"
 s_6="curl -H 'Host: is.snssdk.com' -H 'Cookie: odin_tt=086849e00d3f955d5541126c9a7dec7ec2db27557fdff128f543fda2466a825f2b1ee2b9585686725c960c1e409df588c405e4b8b8be5754b445f00e4af92c8d; d_ticket=5f8fc87635be7acc88e9a5708a3f046607d02; sid_guard=158c6ffe4aa1c03f4d8eef9c7487c6a0%7C1599353104%7C5184000%7CThu%2C+05-Nov-2020+00%3A45%3A04+GMT; uid_tt=0b8e14f094b02cee966fad4d20de14af; uid_tt_ss=0b8e14f094b02cee966fad4d20de14af; sid_tt=158c6ffe4aa1c03f4d8eef9c7487c6a0; sessionid=158c6ffe4aa1c03f4d8eef9c7487c6a0; sessionid_ss=158c6ffe4aa1c03f4d8eef9c7487c6a0; install_id=2374555398325096; ttreq=1$5e844322393483f3a1d4e2d0944a149cc9273d6f' -H 'x-tt-token: 00158c6ffe4aa1c03f4d8eef9c7487c6a06e3c8a43680206e28de7a4277c8847ec1489eef72ab80aa4b50a96f145a84a558' -H 'x-ss-cookie: install_id=2374555398325096; ttreq=1$5e844322393483f3a1d4e2d0944a149cc9273d6f; d_ticket=5f8fc87635be7acc88e9a5708a3f046607d02; odin_tt=086849e00d3f955d5541126c9a7dec7ec2db27557fdff128f543fda2466a825f2b1ee2b9585686725c960c1e409df588c405e4b8b8be5754b445f00e4af92c8d; sessionid=158c6ffe4aa1c03f4d8eef9c7487c6a0; sessionid_ss=158c6ffe4aa1c03f4d8eef9c7487c6a0; sid_guard=158c6ffe4aa1c03f4d8eef9c7487c6a0%7C1599353104%7C5184000%7CThu%2C+05-Nov-2020+00%3A45%3A04+GMT; sid_tt=158c6ffe4aa1c03f4d8eef9c7487c6a0; uid_tt=0b8e14f094b02cee966fad4d20de14af; uid_tt_ss=0b8e14f094b02cee966fad4d20de14af' -H 'tt-request-time: 1601026743581' -H 'user-agent: Super 3.0.6 rv:3.0.6.80 (iPhone; iOS 13.5.1; zh_CN) Cronet' -H 'sdk-version: 2' -H 'passport-sdk-version: 5.12.0' -H 'x-ss-dp: 1319' -H 'x-tt-trace-id: 00-c4a15e910d97fa12ce819a77fe7a0527-c4a15e910d97fa12-01' -H 'x-khronos: 1601026742' -H 'x-gorgon: 840200de0000a98b14af8365890ee23465f1249d4ccb95bf6f38' --compressed 'https://is.snssdk.com/bds/user/publish_list/?iid=2374555398325096&resolution=1242*2208&os_version=13.5.1&app_name=super&channel=App%20Store&idfa=1ABF0A44-4CBE-4191-B8E6-154836171658&device_platform=iphone&mac_address=02:00:00:00:00:00&vid=86A0335D-BB60-42CC-A461-72D1D67C6DE1&openudid=01942689b4d6f20e58ba3abb334a86fcf32d187c&device_type=iPhone%207%20Plus&idfv=86A0335D-BB60-42CC-A461-72D1D67C6DE1&version_code=3.0.6&ac=WIFI&device_id=2673605010266535&aid=1319&update_version_code=30680&cursor=1600863792251060&count=20&user_id=1947990820141172&api_version=1&direction=2'"
 
+kaka1=""
+kaka2=""
+for item in s_4.replace("-H",",\n"):
+    kaka1=kaka1+item
+for item in s_6.replace("-H",",\n"):
+    kaka2=kaka2+item
+
 result=s_5.replace("-H",",\n")
 print(result)
 with open("haha.json","w") as file:
     # file.write(os.system(s_6))
     pass
-print(os.system(s_6))
 
 
+# print(kaka1)
+# print(kaka2)
 # "https://is.snssdk.com/bds/user/publish_list/?iid=2374555398325096&resolution=1242*2208&os_version=13.5.1&app_name=super&channel=App%20Store&idfa=1ABF0A44-4CBE-4191-B8E6-154836171658&device_platform=iphone&mac_address=02:00:00:00:00:00&vid=86A0335D-BB60-42CC-A461-72D1D67C6DE1&openudid=01942689b4d6f20e58ba3abb334a86fcf32d187c&device_type=iPhone%207%20Plus&idfv=86A0335D-BB60-42CC-A461-72D1D67C6DE1&version_code=3.0.6&ac=WIFI&device_id=2673605010266535&aid=1319&update_version_code=30680&count=20&user_id=1947990820141172&api_version=1&direction=1"
 # "https://is.snssdk.com/bds/user/publish_list/?iid=2374555398325096&resolution=1242*2208&os_version=13.5.1&app_name=super&channel=App%20Store&idfa=1ABF0A44-4CBE-4191-B8E6-154836171658&device_platform=iphone&mac_address=02:00:00:00:00:00&vid=86A0335D-BB60-42CC-A461-72D1D67C6DE1&openudid=01942689b4d6f20e58ba3abb334a86fcf32d187c&device_type=iPhone%207%20Plus&idfv=86A0335D-BB60-42CC-A461-72D1D67C6DE1&version_code=3.0.6&ac=WIFI&device_id=2673605010266535&aid=1319&update_version_code=30680&cursor=1600863792251060&count=20&user_id=1947990820141172&api_version=1&direction=2"
 #
-# import difflib
+import difflib
 #
 # text1 = """
 # text1:
@@ -41,7 +49,8 @@ print(os.system(s_6))
 #
 # text2_lines = text2.splitlines()
 #
-# d = difflib.Differ()
-# diff = d.compare(s_4, s_6)
-#
-# print ("".join(list(diff)))
+kaka1_lines=kaka1.splitlines()
+kaka2_lines=kaka2.splitlines()
+d = difflib.Differ()
+diff = d.compare(kaka1_lines, kaka2_lines)
+print ('\n'.join(list(diff)))
